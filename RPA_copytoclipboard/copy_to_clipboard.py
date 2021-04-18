1ef1d31ea3dc6bde449d72680196d21d9c606069
ky8778 <gunyoung8778@gmail.com>
2021-04-18 17:39:50 +0900

    [Copy to Clipboard] Started 1. copy_to_clipboard.py uploaded

---
+++ RPA_copytoclipboard/copy_to_clipboard.py
@@ -0,0 +1,22 @@
+
+from io import BytesIO
+import win32clipboard
+from PIL import Image
+
+def send_to_clipboard(clip_type, data):
+	win32clipboard.OpenClipboard()
+	win32clipboard.EmptyClipboard()
+	win32clipboard.SetClipboardData(clip_type, data)
+	win32clipboard.CloseClipboard()
+
+def copy_to_clipboard(path):
+	BASEPATH = 'images/'
+	print("What is your image file? : ")
+	IMGPATH = input()
+	filepath = BASEPATH + IMGPATH + '.jpg'
+	img = Image.open(filepath)
+	output = BytesIO()
+	img.convert("RGB").save(output, "BMP")
+	data = output.getvalue()[14:] # The file header off-set of BMP is 14bytes
+	output.close()
+	send_to_clipboard(win32clipboard.CF_DIB, data)
---
+++ RPA_copytoclipboard/main_code.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from PIL import ImageTk
+import os
+
+def cmd_ky():
+    print("command Test")
+
+root = Tk()
+root.title("KY")                        # 제목
+root.geometry("600x500+0+0")            # 가로x세로+x+y
+root.resizable(False, False)            # 창 크기 조절 허용 안함
+
+# 위젯(widget) : 버튼, 체크박스 등 이벤트를 통칭한 것을 의미
+btn1 = Button(root, padx=10, pady=10, text="버튼1")
+btn1.pack()                             # btn1 에 설정한 속성 값을 적용시켜줌
+                                        # root에 text로 들어가도록
+
+btn2 = Button(root, fg="red", bg="white", width=10, height=10, text="버튼2")
+btn2.pack()
+
+pjt_dir = os.path.dirname(os.path.realpath(__file__))
+print(pjt_dir) #프로젝트 소스코드 파일 경로 출력
+photo = ImageTk.PhotoImage(file=pjt_dir+"\\images\\img.jpg")
+btn3 = Button(root, image=photo)
+btn3.pack()
+
+btn4 = Button(root, text="cmd btn", command=cmd_ky)
+btn4.pack()
+
+root.mainloop()
